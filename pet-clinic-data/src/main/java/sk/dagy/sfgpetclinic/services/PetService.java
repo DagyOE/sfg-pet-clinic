b28be950ccefa675ee8b9beba127f10c8cf64e8a
Rado / DESKTOP <dugas.radoslav@gmail.com>
2022-08-21 17:02:43 +0200

    Implement Interfaces for service. Closes #5

---
+++ pet-clinic-data/src/main/java/sk/dagy/sfgpetclinic/services/PetService.java
@@ -0,0 +1,12 @@
+package sk.dagy.sfgpetclinic.services;
+
+import sk.dagy.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/sk/dagy/sfgpetclinic/services/VetService.java
@@ -0,0 +1,12 @@
+package sk.dagy.sfgpetclinic.services;
+
+import sk.dagy.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
