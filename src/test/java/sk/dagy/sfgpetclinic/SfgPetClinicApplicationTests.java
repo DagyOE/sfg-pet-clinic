adfd703ca35f99e7f9c3256a13ee57a79a6f36b3
Rado / DESKTOP <dugas.radoslav@gmail.com>
2022-08-21 12:20:33 +0200

    Initial Project Add

---
+++ src/test/java/sk/dagy/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.dagy.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
